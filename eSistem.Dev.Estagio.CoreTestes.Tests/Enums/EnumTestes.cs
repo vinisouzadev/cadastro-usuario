f77f06789e9355f93efb82af278441a560278f19
Vinícius Souza <viniciusouza.dev@gmail.com>
2024-10-24 17:40:50 -0300

    Adiciona arquivos do CoreTestes no gitignore e cria o arquivo do ETipoTestes

---
+++ eSistem.Dev.Estagio.CoreTestes.Tests/Enums/EnumTestes.cs
@@ -0,0 +1,19 @@
+﻿using eSistem.Dev.Estagio.Core.Enums;
+
+namespace eSistem.Dev.Estagio.CoreTestes.Tests.Enums
+{
+    [Trait("Category", "ETipo")]
+    public sealed class EnumTestes
+    {
+        [Fact]
+        public static void Enum_DadoValorDoEnum_EntaoAssocieCorretamente()
+        {
+
+            ETipo tipo = (ETipo)1;
+            ETipo tipo2 = 0;
+
+            Assert.Equal(ETipo.Juridico, tipo);
+            Assert.Equal(ETipo.Fisico, tipo2);
+        }
+    }
+}
