609fc173f78fdb838f4bff8ed60cffe52f87dbe1
Vinícius Souza <viniciusouza.dev@gmail.com>
2024-10-28 16:52:41 -0300

    Adiciona arquivo de teste unitário de RegisterRequest

---
+++ eSistem.Dev.Estagio.CoreTestes.Tests/Requests/Account/RegisterRequestTestes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSistem.Dev.Estagio.CoreTestes.Tests.Requests.Account
+{
+    [Trait("Category", "RegisterRequest")]
+    public class RegisterRequestTestes
+    {
+        public void Construtor_DadoInstanciaSemValores_EntaoDeveSetarPropriedadesDefaultCorretamente()
+        {
+
+        }
+    }
+}
--- eSistem.Dev.Estagio.CoreTestes.Tests/obj/Debug/net8.0/eSistem.Dev.Estagio.CoreTestes.Tests.AssemblyInfo.cs
+++ eSistem.Dev.Estagio.CoreTestes.Tests/obj/Debug/net8.0/eSistem.Dev.Estagio.CoreTestes.Tests.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eSistem.Dev.Estagio.CoreTestes.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19b01b89425189e9d3c7352d03b3c25f23442832")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f92dfa1a5006472b583f4d5b055723b62c330f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("eSistem.Dev.Estagio.CoreTestes.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eSistem.Dev.Estagio.CoreTestes.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
