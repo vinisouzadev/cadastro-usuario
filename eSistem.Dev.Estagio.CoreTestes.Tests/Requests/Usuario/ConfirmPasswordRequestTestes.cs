bc2678f667550c831842f257577524eb3e3524f6
Vinícius Souza <viniciusouza.dev@gmail.com>
2024-10-28 17:16:03 -0300

    Finaliza teste unitário de ConfirmPasswordRequest

--- eSistem.Dev.Estagio.CoreTestes.Tests/Requests/Usuario/ConfirmPasswordRequestTestes.cs
+++ eSistem.Dev.Estagio.CoreTestes.Tests/Requests/Usuario/ConfirmPasswordRequestTestes.cs
@@ -1,8 +1,17 @@
-﻿namespace eSistem.Dev.Estagio.CoreTestes.Tests.Requests.Usuario
+﻿using eSistem.Dev.Estagio.Core.Requests.Usuario;
+using FluentAssertions;
+
+namespace eSistem.Dev.Estagio.CoreTestes.Tests.Requests.Usuario
 {
     [Trait("Category", "ConfirmPasswordRequest")]
     public class ConfirmPasswordRequestTestes
     {
+        [Fact]
+        public void Construtor_DadoInstanciaSemValores_EntaoDeveSetarValoresDefaultCorretamente()
+        {
+            ConfirmedPasswordRequest request = new();
 
+            request.Password.Should().BeEmpty();
+        }
     }
 }
